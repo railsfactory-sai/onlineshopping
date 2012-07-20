f19ff6cf0790dbdc93908afa3ae1ae73da018ed4
sai <saiannadurai@railsfactory.org>
2012-07-20 12:30:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlineshopping::Application.config.secret_token = '0e270f12d76a0cf613f09fbe448724b6fee169710ab467e5450adcf332fc906e696f6a9903e9d4de182cd7677fb4d4837ae6231cdbc06c29416ffdd39a7fcee9'
